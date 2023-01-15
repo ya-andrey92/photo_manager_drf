d614b94bce31dcd7fed0411ef0ffde57e3457826
Andrey Yablonski <ya.andrey92@gmail.com>
2023-01-15 17:56:32 +0300

    Develop models and add settings

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import  static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -37,3 +39,9 @@ urlpatterns = [
     path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
